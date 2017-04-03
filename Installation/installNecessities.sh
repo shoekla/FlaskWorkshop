b5acb2f7ecea96f98a34ab0b87c9d45715c084c6
Abir <abirshukla@gmail.com>
2017-04-03 19:42:20 -0400

    Added sudo

--- Installation/installNecessities.sh
+++ Installation/installNecessities.sh
@@ -3,19 +3,19 @@
 echo "Installing Necessities for Flask Workshop\n"
 BASEDIR=$(dirname "$0")
 cd $BASEDIR
-python getpip.py
-pip install flask
-pip install requests
-pip install bs4
-pip install BeautifulSoup
+sudo python getpip.py
+sudo pip install flask
+sudo pip install requests
+sudo pip install bs4
+sudo pip install BeautifulSoup
 BLUE='\033[1;34m'
-  
+WHITE='\033[1;37m'  
 echo "${BLUE}TESTING INSTALLATION ---------------------------\n"
 python testInstallation.py
 
 
 echo "${BLUE}ASK ORGANIZER If you see an error message above------\n"
-echo "Press enter to exit"
+echo "${WHITE}Press enter to exit"
 read "..."
 
 
